d623eda2856d5392b2f8091f927e02f34145bb6e
__vic <__vic@ngs.ru>
2018-09-20 10:21:17 +0300

    logger::output added

--- src/logger.cpp
+++ src/logger.cpp
@@ -7,17 +7,35 @@
 
 namespace __vic {
 
-const char * const logger::sev_strs[] =
+extern const char * const logger_severity_strs[] =
     { "TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "FATAL" };
+//----------------------------------------------------------------------------
+logger::logger(output &out, severity_t level)
+:
+    log_level(level), out(&out),
+    cur_msg(lo_water_mark), rec_objs_count(0)
+{
+}
+//----------------------------------------------------------------------------
+logger::logger(settings_t s)
+:
+    log_level(s.level()), out(&s.output()),
+    cur_msg(lo_water_mark), rec_objs_count(0)
+{
+}
+//----------------------------------------------------------------------------
+logger::~logger()
+{
+}
 //----------------------------------------------------------------------------
 void logger::message(severity_t s, const char *msg, size_t msg_len)
 {
-    if(s >= level()) publish_record(s, msg, msg_len);
+    if(s >= level()) out->publish_record(s, msg, msg_len);
 }
 //----------------------------------------------------------------------------
 void logger::message(severity_t s, const char *msg)
 {
-    if(s >= level()) publish_record(s, msg, std::strlen(msg));
+    if(s >= level()) out->publish_record(s, msg, std::strlen(msg));
 }
 //----------------------------------------------------------------------------
 void logger::_flush()
@@ -25,7 +43,7 @@ void logger::_flush()
     // Micro-optimization: Avoiding message() fuction call
     //message(cur_severity, cur_msg.data(), cur_msg.length());
     if(cur_severity >= level())
-        publish_record(cur_severity, cur_msg.data(), cur_msg.length());
+        out->publish_record(cur_severity, cur_msg.data(), cur_msg.length());
 
     cur_msg.clear();
     // prevent uncontrolled buffer growth
