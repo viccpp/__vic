c9a869f63eaeb0a4a8bb2c5cd5ec116602bb8fd9
__vic <__vic@ngs.ru>
2019-03-13 12:36:11 +0300

    memory.h and load_unaligned() added

---
+++ include/__vic/memory.h
@@ -0,0 +1,33 @@
+// Memory-related utilities
+//
+// Platform: ISO C++ 98/11
+// $Id$
+//
+// (c) __vic 2019
+
+#ifndef __VIC_MEMORY_H
+#define __VIC_MEMORY_H
+
+#include<__vic/defs.h>
+
+namespace __vic {
+
+//----------------------------------------------------------------------------
+template<class T>
+inline T load_unaligned(const void *p)
+{
+#ifdef __VIC_STRICT_RAM_ALIGNMENT__
+#ifdef __GNUC__
+    struct wrapper { T v; } __attribute__((packed));
+    return static_cast<const wrapper *>(p)->v;
+#else
+#   error Add unaligned access implementation for your compiler/platform
+#endif
+#else // unaligned access is OK
+    return *static_cast<const T *>(p);
+#endif
+}//----------------------------------------------------------------------------
+
+} // namespace
+
+#endif // header guard
