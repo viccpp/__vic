58a4cd8d16409addc7e4b26353e6e472a49c9250
__vic <__vic@ngs.ru>
2020-02-20 15:42:18 +0300

    Renamed object_pool -> fixed_vector

--- include/__vic/fixed_vector.h
+++ include/__vic/fixed_vector.h
@@ -1,152 +1,132 @@
-// A pool controlling objects lifetime
-// Objects are not required be CopyConstructable
-// Pool is organized as stack
+// A vector without autogrowing capacity() can hold non-copyable/movable objects
 //
 // Platform: ISO C++ 98/11
 // $Id$
 //
 // (c) __vic 2007
 
-#ifndef __VIC_OBJECT_POOL_H
-#define __VIC_OBJECT_POOL_H
+#ifndef __VIC_FIXED_VECTOR_H
+#define __VIC_FIXED_VECTOR_H
 
 #include<__vic/defs.h>
 #include<new>
-#include<exception>
 #include __VIC_SWAP_HEADER
 
 namespace __vic {
 
 //////////////////////////////////////////////////////////////////////////////
-// Internal class. Use object_pool<T>::overflow
-struct object_pool_overflow_ : public std::exception
-{
-    const char *what() const noexcept { return "__vic::object_pool: Overflow"; }
-};
-//////////////////////////////////////////////////////////////////////////////
 template<class T>
-class object_pool : private non_copyable
+class fixed_vector : private non_copyable
 {
-    T *pool, *next, *limit;
+    T *mem, *next, *limit;
 
     static T *allocate(size_t n)
         { return static_cast<T *>(::operator new[](sizeof(T) * n)); }
     static void deallocate(T *p) { ::operator delete[](p); }
     static void destroy(T *p) { p->~T(); }
 public:
-    typedef T object_type;
-    typedef object_type value_type; // standard synonym
-    typedef object_type *iterator;
-    typedef const object_type *const_iterator;
-    typedef object_pool_overflow_ overflow;
+    typedef T value_type;
+    typedef value_type *iterator;
+    typedef const value_type *const_iterator;
 
-    object_pool() : pool(0), next(0), limit(0) {}
-    explicit object_pool(size_t );
-    ~object_pool();
+    fixed_vector() : mem(0), next(0), limit(0) {}
+    explicit fixed_vector(size_t );
+    ~fixed_vector();
 
 #if __cpp_rvalue_references
-    object_pool(object_pool &&o) noexcept
-        : pool(o.pool), next(o.next), limit(o.limit)
+    fixed_vector(fixed_vector &&o) noexcept
+        : mem(o.mem), next(o.next), limit(o.limit)
     {
-        o.pool = o.next = o.limit = nullptr;
+        o.mem = o.next = o.limit = nullptr;
     }
-    object_pool &operator=(object_pool &&o) noexcept { swap(o); return *this; }
+    fixed_vector &operator=(fixed_vector &&o) noexcept { swap(o); return *this; }
 #if __cpp_variadic_templates
-    template<class... Args> T &emplace(Args &&... );
+    template<class... Args> T &emplace_back(Args &&... );
 #endif
 #endif
 
     // size in objects
-    size_t size() const { return next - pool; }
-    size_t capacity() const { return limit - pool; }
+    size_t size() const { return next - mem; }
+    size_t capacity() const { return limit - mem; }
     bool full() const { return next == limit; }
-    bool empty() const { return next == pool; }
+    bool empty() const { return next == mem; }
 
     void recreate(size_t , bool = false);
-    void pop()
-    {
-        if(next != pool) destroy(--next);
-    }
+    void pop_back() { destroy(--next); }
     void clear()
     {
         // destruct all objects
-        while(next != pool) destroy(--next);
+        while(next != mem) destroy(--next);
     }
-    void swap(object_pool &o) noexcept
+    void swap(fixed_vector &o) noexcept
     {
-        std::swap(pool, o.pool);
+        std::swap(mem, o.mem);
         std::swap(next, o.next);
         std::swap(limit, o.limit);
     }
     // BEGIN: Deprecated functions. Use emplace() in C++11 mode
     // returns pointer to memory for object allocation
-    void *alloc()
-    {
-        if(full()) throw overflow();
-        return next;
-    }
-    // adds last allocated object to the pool
+    void *alloc() { return next; }
+    // adds last allocated object to the container
     void push_allocated() { next++; }
     // END: Deprecated functions
 
     // element access
-    object_type &operator[](size_t i) { return pool[i]; }
-    iterator begin() { return pool; }
+    T &operator[](size_t i) { return mem[i]; }
+    iterator begin() { return mem; }
     iterator end() { return next; }
-    object_type &front() { return *pool; }
-    object_type &back() { return *(next-1); }
+    T &front() { return *mem; }
+    T &back() { return *(next-1); }
 
-    const object_type &operator[](size_t i) const { return pool[i]; }
-    const_iterator begin() const { return pool; }
+    const T &operator[](size_t i) const { return mem[i]; }
+    const_iterator begin() const { return mem; }
     const_iterator end() const { return next; }
     const_iterator cbegin() const { return begin(); }
     const_iterator cend() const { return end(); }
-    const object_type &front() const { return *pool; }
-    const object_type &back() const { return *(next-1); }
+    const T &front() const { return *mem; }
+    const T &back() const { return *(next-1); }
 };
 //////////////////////////////////////////////////////////////////////////////
 //----------------------------------------------------------------------------
 template<class T>
-object_pool<T>::object_pool(size_t max_size)
+fixed_vector<T>::fixed_vector(size_t max_size)
 {
-    pool = next = this->allocate(max_size);
-    limit = pool + max_size;
+    mem = next = this->allocate(max_size);
+    limit = mem + max_size;
 }
 //----------------------------------------------------------------------------
 template<class T>
-object_pool<T>::~object_pool()
+fixed_vector<T>::~fixed_vector()
 {
     clear();
-    deallocate(pool);
+    deallocate(mem);
 }
 //----------------------------------------------------------------------------
 template<class T>
-void object_pool<T>::recreate(size_t max_size, bool exact)
+void fixed_vector<T>::recreate(size_t max_size, bool exact)
 {
     clear();
     if(max_size > capacity() || (exact && max_size != capacity()))
     {
-        deallocate(pool);
-        pool = next = this->allocate(max_size);
-        limit = pool + max_size;
+        deallocate(mem);
+        mem = next = this->allocate(max_size);
+        limit = mem + max_size;
     }
 }
 //----------------------------------------------------------------------------
 #if __cpp_rvalue_references && __cpp_variadic_templates
 template<class T>
 template<class... Args>
-T &object_pool<T>::emplace(Args &&... args)
+T &fixed_vector<T>::emplace_back(Args &&... args)
 {
-    if(full()) throw overflow();
-    T *obj = ::new(static_cast<void*>(next))
-        object_type(std::forward<Args>(args)...);
+    T *obj = ::new(static_cast<void*>(next)) T(std::forward<Args>(args)...);
     next++;
     return *obj;
 }
 #endif
 //----------------------------------------------------------------------------
 template<class T>
-inline void swap(object_pool<T> &o1, object_pool<T> &o2) noexcept { o1.swap(o2); }
+inline void swap(fixed_vector<T> &o1, fixed_vector<T> &o2) noexcept { o1.swap(o2); }
 //----------------------------------------------------------------------------
 
 } // namespace
--- test/fixed_vector.cpp
+++ test/fixed_vector.cpp
@@ -1,5 +1,5 @@
 #include<__vic/defs.h>
-#include<__vic/object_pool.h>
+#include<__vic/fixed_vector.h>
 #include<iostream>
 #include<exception>
 #include<string>
@@ -17,30 +17,30 @@ public:
 
 void run()
 {
-    __vic::object_pool<C> pool(2);
-    std::cout << "Pool size: " << pool.size() << '\n';
+    __vic::fixed_vector<C> v(2);
+    std::cout << "Vector size: " << v.size() << '\n';
 
-    new(pool.alloc()) C("C1");
-    assert(pool.size() == 0);
-    pool.push_allocated();
-    assert(pool.size() == 1);
+    new(v.alloc()) C("C1");
+    assert(v.size() == 0);
+    v.push_allocated();
+    assert(v.size() == 1);
 
 #if __cpp_rvalue_references && __cpp_variadic_templates
-    pool.emplace("C2");
+    v.emplace_back("C2");
 #else
-    new(pool.alloc()) C("C2");
-    pool.push_allocated();
+    new(v.alloc()) C("C2");
+    v.push_allocated();
 #endif
 
-    assert(pool.size() == 2);
-    std::cout << "Pool size: " << pool.size() << '\n';
-    std::cout << "Pool access by index:\n";
-    for(unsigned i = 0; i < pool.size(); i++)
-        std::cout << pool[i].name() << ' ';
+    assert(v.size() == 2);
+    std::cout << "Vector size: " << v.size() << '\n';
+    std::cout << "Access by index:\n";
+    for(unsigned i = 0; i < v.size(); i++)
+        std::cout << v[i].name() << ' ';
     std::cout << '\n';
-    std::cout << "Pool access by iterator:\n";
-    for(__vic::object_pool<C>::const_iterator it =
-            pool.cbegin(); it != pool.cend(); ++it)
+    std::cout << "Access by iterator:\n";
+    for(__vic::fixed_vector<C>::const_iterator it =
+            v.cbegin(); it != v.cend(); ++it)
         std::cout << it->name() << ' ';
     std::cout << '\n';
 }
